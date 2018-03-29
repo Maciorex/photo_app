e21ea6c62890e99baae49febf79715208cac0232
Maciorex <maciej.zabski@gmail.com>
2018-03-29 21:25:29 +0200

    javascript bug correction

--- app/assets/javascripts/credit_card_form.js
+++ app/assets/javascripts/credit_card_form.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
-
+  console.log("czy ruszam?");
   var show_error, stripeResponseHandler, submitHandler;
 
   submitHandler = function (event) {
      var $form = $(event.target);
+     console.log("mordooo");
      $form.find("input[type=submit]").prop("disabled", true);
 
      //If Stripe was initialized correctly this will create a token using the credit card info
      if(Stripe){
        Stripe.card.createToken($form, stripeResponseHandler);
+        console.log("tutaj w ifie jestem");
      } else {
        show_error("Failed to load credit card processing functionality. Please reload this page in your browser.")
      }
@@ -19,14 +21,14 @@ $(document).ready(function() {
 
   stripeResponseHandler = function (status, response) {
     var token, $form;
-
     $form = $('.cc_form');
-
+    console.log("kupo");
     if (response.error) {
       console.log(response.error.message);
       show_error(response.error.message);
       $form.find("input[type=submit]").prop("disabled", false);
     } else {
+      console.log("dupa zdanexa");
       token = response.id;
       $form.append($("<input type=\"hidden\" name=\"payment[token]\" />").val(token));
       $("[data-stripe=number]").remove();
@@ -36,7 +38,7 @@ $(document).ready(function() {
       $("[data-stripe=label]").remove();
       $form.get(0).submit();
     }
-
+    console.log("dupa adamexa");
     return false;
   };
   show_error = function (message) {
